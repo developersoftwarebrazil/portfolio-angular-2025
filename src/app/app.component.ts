ff0ff7e2e01d7ceae68eb2f78c10d84379d74982
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-05-08 19:20:21 -0300

    feet: create navbar components, imports ims and svgs and  create folder to scss configs for styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio-angular-2025';
+  title = 'portfólio - desenvolvedor designer ';
 }
