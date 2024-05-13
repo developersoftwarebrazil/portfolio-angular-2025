f66c60a7f4bfc1a486ce740aa989f3eb3bd9052f
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2024-05-13 12:23:12 -0300

    implemenented metgod to get year to copyright

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   emailAddress = 'contatodevelopersoftwarebrazildna@gmail.com';
+  currentYears!: number;
+
+  constructor(){}
+  ngOnInit(): void {
+    this.getCurrentYear();
+  }
+
+  getCurrentYear(){
+    this.currentYears = new Date().getFullYear();
+  }
 }
